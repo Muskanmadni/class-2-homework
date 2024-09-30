12a9f6730e8c9586c7604396ed4e01f5ac54df85
umm e habiba madni <88267142+Muskanmadni@users.noreply.github.com>
2024-09-30 02:15:42 -0700

    first commit

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+     <div className="header">
+         <ul className="header-buttons">
+             <li>Home</li>
+             <li>About</li>
+             <li>Contact</li>
+         </ul>
+     </div>     
+    )
+ }
